c97668544eb9a8a2001fed958781c721d9aedb22
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2026-01-12 10:16:58 +0530

    1/5

---
+++ 41_ascci_code.c
@@ -0,0 +1,11 @@
+// print ascci code of a character
+#include <stdio.h>
+void main()
+{
+    char ch;
+    printf("enter a char : ");
+    scanf("%c", &ch);
+    printf("character is %c\n", ch);
+    printf("ascii code  is %d\n ", ch);
+    printf("char is %c\n ", 4);
+}
