99ff6d085e54ca5e355319c63e8a117175a16d08
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-02-13 10:17:08 +0530

    09/02

---
+++ 107_nestedloops.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)//1
+    {
+        for (j = 1; j <= 3; j++)//2
+        {
+            printf("hello world institute\n");
+        }
+        printf("-------------------------\n");
+    }
+}
